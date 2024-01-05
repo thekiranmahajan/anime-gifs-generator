ecad0124df122525b2071bd6eaa242a54b7555d9
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2024-01-05 15:17:33 +0530

    Ready to be deployed✅

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const animePicContainer = document.getElementById("anime-pic-container");
 const options = document.querySelectorAll('input[name="options"]');
 
 const getAnimeGifs = async (category) => {
-  const url = `https://api.waifu.pics/sfw/${category}`;
-  const gif = await fetch(url).then((res) => res.json());
+  try {
+    const url = `https://api.waifu.pics/sfw/${category}`;
+    const gif = await fetch(url).then((res) => res.json());
 
-  animePicContainer.innerHTML += `<div class="card" id="card">
+    animePicContainer.innerHTML += `<div class="card" id="card">
                 <img src="${gif.url}" alt="gif" />
               </div>`;
+  } catch (error) {
+    animePicContainer.innerHTML = `<p style="text-align: center; color: white;" >Oops something went wrong...</br> ${error}</p>`;
+  }
 };
 
 const removeStyles = () => {
